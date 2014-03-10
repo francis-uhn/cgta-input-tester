cc0ef1a8974d5e5b41abc8b184a755100552ceb1
mbodkinuhn@gmail.com <mbodkinuhn@gmail.com@04a203ac-fec2-2902-4949-901a43d85edd>
2014-03-10 15:02:30 +0000

    Added SearchOID GWT lookup for populating Hospital drop-down select boxes from the table_9004.csv CSV input file (no hard-coding of OIDs)

---
+++ trunk/CGTA_Data_Viewer/src/ca/cgta/dataviewer/server/SearchOIDServiceImpl.java
@@ -0,0 +1,56 @@
+package ca.cgta.dataviewer.server;
+
+import java.io.File;
+import java.io.InputStream;
+
+import au.com.bytecode.opencsv.CSV;
+import au.com.bytecode.opencsv.CSVReadProc;
+import ca.cgta.dataviewer.client.SearchOIDService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+
+/**
+ * The server-side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class SearchOIDServiceImpl extends RemoteServiceServlet implements
+		SearchOIDService {
+	
+	private static final CSV csv = CSV
+            .separator(';')
+            .quote('\'')
+            .skipLines(1)
+            .charset("UTF-8")
+            .create();
+	
+	private String returnOIDs = "";
+	
+	public String searchOIDServer(String input)  throws IllegalArgumentException {
+		
+		String fileName = ""; // C:\\Users\\t33377uhn\\workspace\\cgta-input-tester\\CGTA_Data_Viewer\\table_9004.csv";
+		
+		//	System.out.println(this.class.getclassloader.getresource("");
+		// System.out.println("System path: " + );
+	    
+		File f = new File(SearchOIDServiceImpl.class.getClassLoader().getResource("").getPath());
+		String path = f.getParent();
+		fileName = path.substring(path.lastIndexOf("\\")+1,path.length()) + File.separator + "table_9004.csv";
+		System.out.println(fileName);  
+		
+		// CSVReader will be closed after end of processing
+        // Less code to process CSV content -> less bugs
+        csv.read(fileName, new CSVReadProc() {
+                public void procRow(int rowIndex, String... values) {
+                	//	returnOIDs.concat(values[0] + ":");
+                	String[] csvSplit = values[0].split(",");
+                	returnOIDs = returnOIDs + csvSplit[0] + "@" + csvSplit[1] + ":";
+                	//	System.out.println(rowIndex + "# " + Arrays.asList(values));    
+                }
+        });
+		
+        //	System.out.println("Returning string " + returnOIDs); 
+        return returnOIDs;
+	}
+
+}
